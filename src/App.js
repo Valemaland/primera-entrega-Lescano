ef0789a39cd7c0efee38ee102252014f75beacf2
Valema <valerialescano@gmail.com>
2021-03-26 18:59:40 -0300

    primera entrega en git

--- src/App.js
+++ src/App.js
@@ -7,15 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>-Librería espacializada en ilustración-</code>
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          
         >
-          Learn React
+          Libro Liebre 
         </a>
       </header>
     </div>
