d472aee1d662515b63389a498574cb824dfc20d8
Valema <valerialescano@gmail.com>
2021-03-29 23:13:55 -0300

    version2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+/*import './NavBar.js';*/
 function App() {
   return (
     <div className="App">
@@ -9,13 +9,48 @@ function App() {
         <p>
           <code>-Librería espacializada en ilustración-</code>
         </p>
-        <a
-          
-        >
+        <p>
           Libro Liebre 
-        </a>
+        </p>
+      
+      <nav className="navbar" >
+            <ul>
+                <li>
+                    
+                    <a href="#">
+                    Nosotros                       
+                    </a>
+                    
+                </li>
+                <li>
+                    
+                    <a href="https://www.google.com">   
+                    Tienda
+                    </a>
+                </li>
+                <li>
+                    
+                    <a href="https://ar.pinterest.com/srtamedusa/libros-ilustrados/">  
+                    Blog                      
+                    </a>
+                </li>
+                <li>
+                    
+                    <a href="#/">  
+                    Eventos                     
+                    </a>
+                </li>
+                <li>
+                      
+                    <a href="#">      
+                    Contacto               
+                    </a>
+                </li>
+            </ul>
+        </nav>
       </header>
     </div>
+    
   );
 }
 
---
+++ src/NavBar.js
@@ -0,0 +1,42 @@
+/*
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+function app() {
+    return (
+        <nav className="navbar" class="navbar navbar-expand-sm bg-light">
+            <ul>
+                <li>
+                    <a>
+                        href="https://ar.pinterest.com/srtamedusa/libros-ilustrados/"
+                        Nosotros
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        href="https://ar.pinterest.com/srtamedusa/libros-ilustrados/"
+                        Tienda
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        href="https://ar.pinterest.com/srtamedusa/libros-ilustrados/"
+                        Blog
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        href="https://ar.pinterest.com/srtamedusa/libros-ilustrados/"
+                        Eventos
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        href="https://ar.pinterest.com/srtamedusa/libros-ilustrados/"
+                        Contacto
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
+
+}
+*/
